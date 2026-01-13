476969ba9c56530f0f4956d87fb979e78647cef2
JJ Garzella <jjgarzella@gmail.com>
2026-01-13 06:52:40 -0800

    Restructure Karatsuba files
    Also add some karatsuba kernels

---
+++ src/KaratsubaMatrix/KaratsubaKernels.jl
@@ -0,0 +1,145 @@
+
+@inline function karatsuba_add_helper(a1,a2,b1,b2,N1,N2)
+
+    temp = (a1 + b1) % (2*N1)
+    temp = div(temp,N1)
+    res2 = (temp + a2) % (2*N2)
+    res2 = (res2 + b2) % (2*N2)
+
+    temp = (temp * N1) % (N1^2)
+    temp = (b1 - temp) % (N1^2)
+    res1 = (a1 + temp) % (N1^2)
+
+    res1 %= N1
+    res2 %= N2
+
+    (res1, res2)
+end
+
+"""
+
+Works even for multidimensional arrays because CuArrays support linear indexing
+
+"""
+function karatsuba_add_kernel!(Kdata1,Kdata2,Adata1,Adata2,Bdata1,Bdata2,N1,N2)
+    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
+    (res1, res2) = karatsuba_add_helper(Adata1[i], 
+                                        Adata2[i], 
+                                        Bdata1[i], 
+                                        Bdata2[i], 
+                                        N1, 
+                                        N2)
+
+    # Kplan[i] = (Adata1[i] + Bdata1[i]) % (2*N1)
+    # Kplan[i] = div(Kplan[i],N1)
+    # Kdata2[i] = (Kplan[i] + Adata2[i]) % (2*N2)
+    # Kdata2[i] = (Kdata2[i] + Bdata2[i]) % (2*N2)
+
+    # Kplan[i] = (Kplan[i] * N1) % (N1^2)
+    # Kplan[i] = (Bdata1[i] - Kplan[i]) % (N1^2)
+    # Kdata1[i] = (Adata1[i] + Kplan[i]) % (N1^2)
+
+    # Kdata1[i] %= N1
+    # Kdata2[i] %= N2
+
+    Kdata1[i] = res1
+    Kdata2[i] = res2
+
+    return
+end
+
+@inline function karatsuba_scalar_multiply_helper(a1,a2,s,N1,N2)
+    res2 = (a1*s) % (N1^2)
+    res2 = div(res2, N1)
+    res1 = (a2*s) % N2
+    res2 = (res2 + res1) % N2
+    res1 = (a1 * s) % N1
+
+    (res1, res2)
+end
+
+function karatsuba_scalar_multiply_kernel!(Bdata1,Bdata2,Adata1,Adata2,s,N1,N2)
+    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
+    # Bdata2[i] = (Adata1[i]*s) % (N1^2)
+    # Bdata2[i] = div(Bdata2[i], N1)
+    # Bdata1[i] = (Adata2[i]*s) % N2
+    # Bdata2[i] = (Bdata2[i] + Bdata1[i]) % N2
+    # Bdata1[i] = (Adata1[i] * s) % N1
+    
+    (res1, res2) = karatsuba_scalar_multiply_helper(Adata1[i],Adata2[i],s,N1,N2)
+    
+    Bdata1[i] = res1
+    Bdata2[i] = res2
+
+    nothing
+end
+
+@inline function karatsuba_negate_kernel_helper(a1,a2,N1,N2,M)
+    res2 = (zero(eltype(Kdata2))) % (N2)
+    res2 = (res2 + N1) % (2*N1)
+    res2 = (res2 - a1) % (2*N1)
+
+    res2 = div(res2, N1)
+    res1 = (res2 * N1) % (M^2)
+    res1 = (res1 + a1) % (M^2)
+    res1 = (-res1) % N1
+
+    res1 = mod(res1, N1)
+
+    res2 = (res2 + N2) % (M^2)
+    res2 = (res2 - 1) % (M^2)
+    res2 = (res2 - a2) % M
+    res2 %= N2
+
+    # (res1, res2)
+end
+
+function karatsuba_negate_kernel!(Kdata1,Kdata2,Adata1,Adata2,N1,N2,M)
+    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
+    # res1, res2 = karatsuba_negate_kernel_helper(Adata1[i],
+    #                                             Adata2[i],
+    #                                             N1,
+    #                                             N2,
+    #                                             M)
+
+    # Kdata2[i] = (Kdata2[i] * 0) % N1
+    # Kdata2[i] = N1
+    # @cuprintf("i=%d, Kdata2[i]=%f", i, Kdata2[i])
+    # Kdata2[i] = 0.0
+    # a1 = Adata1[i]
+    # a2 = Adata2[i]
+
+    Kdata2[i] = (zero(eltype(Kdata2))) % (N2)
+    Kdata2[i] = (Kdata2[i] + N1) % (2*N1)
+    Kdata2[i] = (Kdata2[i] - Adata1[i]) % (2*N1)
+
+    Kdata2[i] = div(Kdata2[i], N1)
+    Kdata1[i] = (Kdata2[i] * N1) % (M^2)
+    Kdata1[i] = (Kdata1[i] + Adata1[i]) % (M^2)
+    Kdata1[i] = (-Kdata1[i]) % N1
+
+    Kdata1[i] = mod(Kdata1[i], N1)
+
+    Kdata2[i] = (Kdata2[i] + N2) % (M^2)
+    Kdata2[i] = (Kdata2[i] - 1) % (M^2)
+    Kdata2[i] = (Kdata2[i] - Adata2[i]) % M
+    Kdata2[i] %= N2
+    
+    # Kdata1[i] = res1
+    # Kdata2[i] = res2
+
+    nothing
+end
+
+function karatsuba_sub_kernel!(Kdata1,Kdata2,Adata1,Adata2,Bdata1,Bdata2,N1,N2,M)
+
+    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
+    bneg1, bneg2 = karatsuba_negate_kernel_helper(Bdata1[i],Bdata2[i],N1,N2,M)
+    
+    res1, res2 = karatsuba_add_helper
+
+end
